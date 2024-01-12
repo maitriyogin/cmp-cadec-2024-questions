cfb3a960a20a5256455e32e3d9b1b689b28829b5
maitriyogin <stephen.white@volvocars.com>
2024-01-12 17:50:32 +0100

    fix: admin page

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -11,48 +11,45 @@ export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData();
   let id = Number(form.get("delete") ?? -1);
   if (id === -1) {
-    const id = Number(form.get("vote"));
+    const id = Number(form.get("enable"));
 
     const q = await db.questions.findUnique({ where: { id } });
     if (q !== undefined && q !== null) {
-      const fields = { votes: q.votes + 1 };
+      const fields = { allowed: !q.allowed };
       await db.questions.update({ where: { id }, data: fields });
     }
   } else {
     await db.questions.delete({ where: { id } });
   }
-  return redirect(`/`);
+  return redirect(`/admin`);
 };
 
 export const loader: LoaderFunction = async () => {
   const data: LoaderData = {
-    questions: await db.questions.findMany({
-      orderBy: { votes: "desc" },
-      where: { allowed: true },
-    }),
+    questions: await db.questions.findMany({ orderBy: { votes: "desc" } }),
   };
   return json(data);
 };
-export default function _index() {
+export default function admin() {
   const data = useLoaderData<LoaderData>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Cadec 2024 Questions!</h1>
-      <a href="/new">Add a new question</a>
+      <h1>Admin</h1>
       <p> Questions: </p>
+      <a href="/">Back to questions</a>
       <Form method="post">
         <ul>
-          {data.questions.map(({ id, question, talkId, votes }) => (
+          {data.questions.map(({ id, question, talkId, votes, allowed }) => (
             <li key={id}>
               <span>{votes}</span>
               <button
                 type="submit"
-                name={"vote"}
+                name={"enable"}
                 className="button"
                 value={id}
                 style={{ margin: 4 }}
               >
-                Vote
+                {allowed ? "Approved" : "Pending"}
               </button>
               <button
                 type="submit"
